5c54cf6567018b0b625b4a50c44f8bfb399809c3
JEEL SADARIYA <144840020+Zeel555@users.noreply.github.com>
2025-07-27 22:54:04 +0530

    Delete client directory

--- client/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/', // ✅ For correct asset loading in Netlify
-});
