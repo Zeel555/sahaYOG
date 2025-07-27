afddd6c170d1168eb70dc5b130215674873135ed
Zeel Sadariya <zeelsadariya@gmail.com>
2025-07-27 21:55:55 +0530

    done

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -21,7 +21,7 @@ const LoginPage = () => {
       const res = await fetch("${import.meta.env.VITE_API_URL}/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(formData),
+        body: JSON.stringify(email,password,role),
       });
       const data = await res.json();
       if (!res.ok) throw new Error(data.message || "Login failed");
